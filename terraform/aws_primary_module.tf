ed5e722df4b1dff84523a4403b969b03a2405508
Maksym Kuzmentsov <kuzmentsov@gmail.com>
2024-09-28 05:01:45 +0300

    Completed

--- terraform/aws_primary_module.tf
+++ terraform/aws_primary_module.tf
@@ -1,5 +1,5 @@
-# module "aws_primary_module" {
-#   source = "./aws/modules/primary"
+module "aws_primary_module" {
+  source = "./aws/modules/primary"
   
-#   ec2_instance_type = var.ec2_instance_type
-# }
+  ec2_instance_type = var.ec2_instance_type
+}
