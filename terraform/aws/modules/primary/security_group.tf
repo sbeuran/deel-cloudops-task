d459faf31d038558f4560d9a3ce6a3ca478c40b0
Maksym Kuzmentsov <kuzmentsov@gmail.com>
2024-09-28 05:18:08 +0300

    Completed

--- terraform/aws/modules/primary/security_group.tf
+++ terraform/aws/modules/primary/security_group.tf
@@ -16,11 +16,15 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.allow_http_https.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  from_port = 80
+  to_port = 80
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.allow_http_https.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  from_port = 443
+  to_port = 443
+  ip_protocol       = "tcp"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-# output "primary_ec2_public_ip" {
-#   value = module.aws_primary_module.primary_ec2_public_ip
-# }
+output "primary_ec2_public_ip" {
+  value = module.aws_primary_module.primary_ec2_public_ip
+}
 
-# output "primary_task_ssh_private_key" {
-#   value     = module.aws_primary_module.primary_task_ssh_private_key
-#   sensitive = true
-# }
+output "primary_task_ssh_private_key" {
+  value     = module.aws_primary_module.primary_task_ssh_private_key
+  sensitive = true
+}
 
 output "secondary_ec2_public_ip" {
   value = module.aws_secondary_module.secondary_ec2_public_ip
