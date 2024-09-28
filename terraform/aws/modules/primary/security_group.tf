a6125c2e5ade4acf78255e07ea731d0646ef7e1a
Maksym Kuzmentsov <kuzmentsov@gmail.com>
2024-09-28 05:12:05 +0300

    Completed

--- terraform/aws/modules/primary/security_group.tf
+++ terraform/aws/modules/primary/security_group.tf
@@ -16,11 +16,11 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.allow_http_https.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "http"
+  ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.allow_http_https.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "https"
+  ip_protocol       = "-1"
 }
